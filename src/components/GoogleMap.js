421a8da5c49a93e42d1fcc69947d37625638d522
moxdev <moxdev1@gmail.com>
2018-12-14 09:36:31 -0500

    got google-map-react working, going to try react-ggole-maps because it has better docs

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -9,25 +9,25 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
 class SimpleMap extends Component {
   static defaultProps = {
     center: {
-      lat: 59.95,
-      lng: 30.33
+      lat: 39.130120,
+      lng: -76.792479
     },
-    zoom: 11
+    zoom: 18
   };
 
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div style={{ height: '500px', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyBoWrSHgKv9M1pBU5NebahO66UtQPsIUkI' }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
           <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={'Kreyser Avrora'}
+            lat={39.130120}
+            lng={-76.792479}
+            text={'Millennium'}
           />
         </GoogleMapReact>
       </div>
